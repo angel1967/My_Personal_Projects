5a5d565d4a4388876fb4575fa60bf4f9b637cec5
miguelcastaneda <castaned@mx1.ibm.com>
2022-06-20 16:45:50 -0500

    Adding CRUD functionality

--- Bootcamp.js
+++ Bootcamp.js
@@ -2,4 +2,25 @@
 // Alumno: Miguel Angel Castañeda Aguilera
 // Fecha: 20 Junio 2022
 console.log('My program is working');
-// Dev branch added
+// Dev branch added ==================================
+const express = require('express')
+var app = express()
+var mongoose = require('mongoose');
+// Router
+const router = require('./routes/emp')
+app.use('/emp',router)
+//Route
+app.get('/',function(req,res){
+  res.send('hello world')
+})
+//MongoDB connection
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+mongoose.connection.once('open',function(){
+  console.log('Database connected Successfully');
+}).on('error',function(err){
+  console.log('Error', err);
+})
+//Server
+app.listen(8000,function(){
+  console.log('Server is Up')
+})
