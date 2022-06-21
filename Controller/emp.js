c405e52373d3a23894361280fe96bff77230d3da
miguelcastaneda <castaned@mx1.ibm.com>
2022-06-21 00:07:23 -0500

    update create new record if not found

--- Controller/emp.js
+++ Controller/emp.js
@@ -1,7 +1,7 @@
 // Controller
 const Emp = require('../models/emp')
 const mongoose = require('mongoose')
-function create(req,res,next){
+function create(req,res,next,resupdate=true){
   let empName = req.body.empName;
   let empEmail = req.body.empEmail;
   let empMobile = req.body.empMobile;
@@ -10,9 +10,15 @@ function create(req,res,next){
     empEmail,
     empMobile
   })
+ 
   emp.save().then((data)=>{
-    res.send(data)
-  })
+
+    if (resupdate){
+    res.send(data)}
+  }).catch(err => {
+    // Some code if promise action failed
+    console.log(err)
+})
 }
 function view(req,res,next){
     Emp.find({}).then((data)=>{
@@ -23,16 +29,17 @@ function view(req,res,next){
 function update(req,res,next){
     Emp.findByIdAndUpdate(req.params.id,req.body, (err,emp)=>{
       if (err) {
-        create(req,res,next)
-        return res.status(201).send({error: "Problem with Updating, new record created "})
-      };
-      res.status(200).send({success: "Updating successfull"});
+    
+        create(req,res,next,false)
+        res.status(201).send({success: "new record created"})
+      }else{
+      res.status(200).send({success: "Updating successfully"});}
     })
   }
 function remove(req,res,next){
     Emp.findByIdAndDelete(req.params.id, (err,emp)=>{
       if(err){
-        return res.status(500).send({error: "Problem with Deleting the Employee recored "})
+        return res.status(500).send({error: "Problem with Deleting the Employee record "})
       }
       res.send({success: 'Employee deleted successfully'})
     })
